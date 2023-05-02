125ee0db8db1661b85335bcd4ede3d5ccd0f94e8
Bryan <bmaravilla@myseneca.ca>
2023-05-02 16:36:10 -0400

    added more tests

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import "./Header.css"
+import React from "react";
+import "./Header.css";
 
-export default function Header({
-    title
-}) {
+export default function Header({ title }) {
     return (
         <>
-            <h1 title="Header" className="header">{title}</h1>
-            {/* <h3 data-testid="header-2" className="header">Hello</h3> */}
+            <h1 className="header">{title}</h1>
+            {/* <h3 title="Header" data-testid="Header-3" className="header">
+                Hello
+            </h3> */}
         </>
-    )
+    );
 }
---
+++ src/components/Header/__test__/Header.test.js
@@ -0,0 +1,54 @@
+import { render, screen } from "@testing-library/react";
+import Header from "../Header";
+
+it("should render header with passed props", () => {
+    render(<Header title="My Header" />);
+    const headerElement = screen.getByText(/My Header/i);
+    expect(headerElement).toBeInTheDocument();
+});
+
+// it("should render header with passed props", () => {
+//     render(<Header title="My Header" />);
+//     const headerElement = screen.getByRole("heading");
+//     expect(headerElement).toBeInTheDocument();
+// });
+
+// it("should render header with passed props", () => {
+//     render(<Header title="My Header" />);
+//     // property name is case sensitive and it represents the text of the element
+//     const headerElement = screen.getByRole("heading", { name: "My Header" });
+//     expect(headerElement).toBeInTheDocument();
+// });
+
+// it("should render header with passed props", () => {
+//     render(<Header title="My Header" />);
+//     const headerElement = screen.getByTitle("Header");
+//     expect(headerElement).toBeInTheDocument();
+// });
+
+// it("should render header with passed props", () => {
+//     render(<Header title="My Header" />);
+//     const headerElement = screen.getByTestId("Header-3");
+//     expect(headerElement).toBeInTheDocument();
+// });
+
+// // Find By
+// it("should render header with passed props", async () => {
+//     render(<Header title="My Header" />);
+//     const headerElement = await screen.findByText(/My Header/i);
+//     expect(headerElement).toBeInTheDocument();
+// });
+
+// // Query By
+// it("should render header with passed props", async () => {
+//     render(<Header title="My Header" />);
+//     const headerElement = screen.queryByText(/dogs/i);
+//     expect(headerElement).not.toBeInTheDocument();
+// });
+
+// // Get All By
+// it("should render header with passed props", async () => {
+//     render(<Header title="My Header" />);
+//     const headerElements = screen.getAllByRole("heading");
+//     expect(headerElements.length).toBe(2);
+// });
